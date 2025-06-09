25f49c706107647591a01f915d888ddd962f5bc8
ArturFalavinha <artur.falavinha@gmail.com>
2025-06-09 19:41:23 -0300

    olá meus manos

---
+++ projetofinallpoo/src/main/java/lpoo/projetofinallpoo/Projetofinallpoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lpoo.projetofinallpoo;
+
+/**
+ *
+ * @author artur
+ */
+public class Projetofinallpoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
